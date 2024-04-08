128f6fe6afef50843855f937a475b1a7e2025e0a
SilvaRuan16 <ruan.gustavo.soares16@gmail.com>
2024-04-08 20:22:29 -0300

    Importação de bibliotecas e continuação

---
+++ packages/database_repository/lib/src/residuo_repository.dart
@@ -0,0 +1,6 @@
+import 'package:database_repository/database_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+/// {@template residuo_repository}
+/// Thrown during the sign up process if a failure occurs.
+/// {@endtemplate}
